a732c8c74e4dd94f4cc01f69908ad8ef4833aea1
Zhantao Yang <mervynyang26@gmail.com>
2021-07-05 18:05:39 +0800

    added bottleneck

--- blocks.py
+++ blocks.py
@@ -80,7 +80,7 @@ class GhostConv(layers.Layer):
                                    padding=padding,
                                    use_bias=not intermediate_norm)
 
-        self.conv2 = keras.Sequential([])
+        self.conv2 = keras.Sequential()
         if intermediate_norm:
             self.conv2.add(intermediate_norm)
         self.conv2.add(layers.Activation(activation))
@@ -173,13 +173,15 @@ class MBBlock(layers.Layer):
                  dropout=.1,
                  drop_connect=.1):
         super(MBBlock, self).__init__()
+        if (strides == 1 or strides == (1, 1)) and in_channels != out_channels:
+            raise ValueError
         self.expand = ConvNormAct(filters=self._round(in_channels * expansion),
                                   kernel_size=1,
                                   activation=activation)
         self.depthwise = ConvNormAct(kernel_size=kernel_size,
                                      strides=strides,
                                      padding="same",
-                                     depthwise=True) if not fused else keras.Sequential()
+                                     depthwise=True) if not fused else lambda val: val
         self.se = keras.Sequential()
         if dropout != 0:
             if dropout_type is None or dropout_type == "dropout":
@@ -193,17 +195,16 @@ class MBBlock(layers.Layer):
                            reduction_ratio=se_reduction,
                            activation1=activation,
                            activation2=se_prob_act))
-        if drop_connect > 0 and strides == 1 and in_channels == out_channels:
-            self.add = tfa.layers.StochasticDepth(survival_probability=1 - drop_connect)
-        else:
-            self.add = layers.Add()
+        self.add = tfa.layers.StochasticDepth(1 - drop_connect) if drop_connect > 0 else layers.Add()
         self.project = ConvNormAct(filters=out_channels,
                                    kernel_size=kernel_size if fused else 1,
                                    activation=activations.linear,
                                    padding="same",
                                    strides=2 if fused and strides == 2 else 1)
         self.shortcut = SkipConnect_d(out_channels=out_channels) if ((strides == 2 or strides == (2, 2))
-                                                                     and use_skipconnect_d) else keras.Sequential()
+                                                                     and use_skipconnect_d) else lambda val: val
+        self.use_skipconnect_d = use_skipconnect_d
+        self.strides = strides
 
     def _round(self, num, divisor=8):
         if num % divisor != 0:
@@ -216,6 +217,54 @@ class MBBlock(layers.Layer):
         x = self.depthwise(x)
         x = self.se(x)
         x = self.project(x)
-        inputs = self.shortcut(inputs)
-        x = self.add([inputs, x])
+        if (self.strides == 1 or self.strides == (1, 1)) or self.use_skipconnect_d:
+            inputs = self.shortcut(inputs)
+            x = self.add([inputs, x])
         return x
+
+
+class bottleneck(layers.Layer):
+    def __init__(self,
+                 filters,
+                 kernel_size=3,
+                 strides=1,
+                 reduction=4,
+                 use_skipconnect_d=True,
+                 se_reduction=4,
+                 se_prob_act=activations.sigmoid,
+                 activation=activations.relu,
+                 drop_connect=.1):
+        super(bottleneck, self).__init__()
+        self.add = tfa.layers.StochasticDepth(1 - drop_connect) if drop_connect > 0 else layers.Add()
+        self.shortcut = SkipConnect_d(out_channels=filters) if ((strides == 2 or strides == (2, 2))
+                                                                and use_skipconnect_d) else keras.Sequential()
+        if strides == 2 or strides == (2, 2):
+            self.shortcut = SkipConnect_d(out_channels=filters) if use_skipconnect_d else layers.Conv2D(filters=filters,
+                                                                                                        kernel_size=1,
+                                                                                                        strides=2)
+        else:
+            self.shortcut = lambda val: val
+        self.conv1 = ConvNormAct(filters=filters // reduction, kernel_size=1, activation=activation)
+        self.conv2 = ConvNormAct(filters=filters // reduction,
+                                 kernel_size=kernel_size,
+                                 strides=strides,
+                                 padding="same",
+                                 activation=activation)
+        self.conv3 = keras.Sequential([
+            layers.Conv2D(filters=filters, kernel_size=1),
+            layers.BatchNormalization()
+        ])
+        self.activation = layers.Activation(activation)
+        self.se = SE(in_channels=filters,
+                     reduction_ratio=se_reduction,
+                     activation1=activation,
+                     activation2=se_prob_act) if se_reduction > 0 else lambda val: val
+
+    def call(self, inputs, *args, **kwargs):
+        x = self.conv1(inputs)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.se(x)
+        shortcut = self.shortcut(inputs)
+        x = self.add([shortcut, x])
+        return self.activation(x)
